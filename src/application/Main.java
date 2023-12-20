702979e2298de62b880ddbf09c511a1dc699fea5
mrtuandao <tuansinhnam2003@gmail.com>
2023-12-20 23:44:08 +0700

    Update login template, add forgot button

--- src/application/Main.java
+++ src/application/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
 //			scene.getStylesheets().add("source/nhankhau_va_hokhau.css");
 
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 
 
--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,6 +1,10 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,38 +12,75 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import static services.MysqlConnection.getMysqlConnection;
 
 public class LoginController {
 	@FXML
 	private TextField tfUsername;
 	@FXML
 	private PasswordField tfPassword;
-	
-	public void Login(ActionEvent event) throws IOException {
-		String name = tfUsername.getText();
-		String pass = tfPassword.getText();
 
-		// check username and password
-		if(!name.equals("") || !pass.equals("")) {
-			Alert alert = new Alert(AlertType.WARNING, "Bạn nhập sai mật khẩu rồi hihi!", ButtonType.OK);
+	@FXML
+	private Label forgot_label;
+
+	int count_times_wrong_acc = 0;
+	public void Login(ActionEvent event) throws IOException,SQLException, ClassNotFoundException {
+		String username = tfUsername.getText();
+		String password = tfPassword.getText();
+		Connection conn = getMysqlConnection();
+
+		// SQL query to check credentials
+		String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+		PreparedStatement preparedStatement = conn.prepareStatement(sql);
+		preparedStatement.setString(1, username);
+		preparedStatement.setString(2, password);
+
+		// Execute the query
+		ResultSet resultSet = preparedStatement.executeQuery();
+		boolean have_first_row = resultSet.next();
+
+		if (have_first_row) {
+			showMainMenu(event);
+		}
+		else {
+			count_times_wrong_acc += 1;
+			Alert alert = new Alert(AlertType.WARNING, "Bạn nhập sai tài khoản hoặc mật khẩu rồi !", ButtonType.OK);
 			alert.setHeaderText(null);
 			alert.showAndWait();
-			return;
+			if (count_times_wrong_acc == 3) {
+				count_times_wrong_acc = 0;
+				forgot_label.setVisible(true);
+			}
 		}
-		
-		Parent home = FXMLLoader.load(getClass().getResource("/views/Home.fxml"));
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-		Scene scene = new Scene(home);
 
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
 	}
 
+	void showMainMenu(ActionEvent event) throws IOException {
+		Parent home = FXMLLoader.load(getClass().getResource("/views/Home.fxml"));
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(home);
+
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
+	}
+
+	@FXML
+	void create_acc_clicked(MouseEvent event) {
+		Alert alert = new Alert(AlertType.WARNING, "Tính năng đang phát triển!!!", ButtonType.OK);
+		alert.setHeaderText(null);
+		alert.showAndWait();
+	}
+
+	@FXML
+	void forgot_clicked(MouseEvent event) {
+		Alert alert = new Alert(AlertType.WARNING, "Tính năng đang phát triển!!!", ButtonType.OK);
+		alert.setHeaderText(null);
+		alert.showAndWait();
+	}
 }
