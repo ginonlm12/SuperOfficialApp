1ff3a1b5b4e03a67ea20601b699a19d98eddae6e
mrtuandao <tuansinhnam2003@gmail.com>
2023-12-20 11:09:55 +0700

    Sua giao dien home

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 //	    	Parent login = FXMLLoader.load(getClass().getResource("/views/Login.fxml"));
-//			Scene scene = new Scene(login,800,600);
+//			Scene scene = new Scene(login,800,650);
 //			scene.getStylesheets().add("source/login.css");
 
 			Parent login = FXMLLoader.load(getClass().getResource("/views/home.fxml"));
